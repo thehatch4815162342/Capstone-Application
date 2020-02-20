080558e0fe13ace4682b1ce85f55bf284b07a679
thehatch4815162342 <sdubois@kent.edu>
2020-02-20 15:38:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/capstoneapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstoneapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
