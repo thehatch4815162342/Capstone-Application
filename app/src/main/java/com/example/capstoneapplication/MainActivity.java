b63d39b87b1fddca176a4982639441c7e4ac94d9
thehatch4815162342 <sdubois@kent.edu>
2020-03-02 19:13:52 -0500

    Example of linking activities

--- app/src/main/java/com/example/capstoneapplication/MainActivity.java
+++ app/src/main/java/com/example/capstoneapplication/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.capstoneapplication;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity2();
+            }
+        });
+    }
+
+    public void openMainActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
-//change
